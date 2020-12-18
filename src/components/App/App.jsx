5f420998c28dd1664618fd6d5f98bb7e1f930abd
DavidStewartLDN <davidstewart.work@gmail.com>
2020-12-18 19:09:48 +0000

    Tests passing for Calculator component to appear in App
    And shallow render test passing for Calculator

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import Calculator from '../Calculator/Calculator';
 import './App.css'
 
-const App = () => <div classname="app-container" />;
+const App = () => (
+  <div classname="app-container">
+    <Calculator />
+  </div>
+);
 
 export default App;

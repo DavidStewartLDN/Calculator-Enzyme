d81ac1740c5d2f9bdd1aa25f651f3fcb5e75df1a
DavidStewartLDN <davidstewart.work@gmail.com>
2020-12-19 19:19:50 +0000

    Added keypad component
    Test passing for the Keypad in Calculator and shallow render test

--- src/components/Calculator/Calculator.spec.js
+++ src/components/Calculator/Calculator.spec.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Calculator from './Calculator';
 import Display from '../Display/Display';
+import Keypad from '../Keypad/Keypad';
 
 describe('Calculator', () => {
   let wrapper;
@@ -13,8 +14,15 @@ describe('Calculator', () => {
   })
 
   it('should render the Display Component', () => {
-    expect(wrapper.containsMatchingElement(
-      <Display displayValue={wrapper.instance().state.displayValue} />
-    )).toEqual(true);
+    expect(wrapper.containsAllMatchingElements([
+      <Display displayValue={wrapper.instance().state.displayValue} />,
+      <Keypad
+        callOperator={wrapper.instance().callOperator}
+        numbers={wrapper.instance().state.numbers}
+        operators={wrapper.instance().state.operators}
+        setOperator={wrapper.instance().setOperator}
+        updateDisplay={wrapper.instance().updateDisplay}
+      />
+    ])).toEqual(true);
   })
 })

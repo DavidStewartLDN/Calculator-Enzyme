5ff7ab9947ea2bbc3783a05df46e97b932f03e00
DavidStewartLDN <davidstewart.work@gmail.com>
2020-12-19 19:49:34 +0000

    Number keys test passing for returning key values

--- src/components/Keypad/Keypad.jsx
+++ src/components/Keypad/Keypad.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Keypad = ({ }) => (
-  <div classname='keypad-container' />
-)
+const Keypad = ({ 
+  callOperator,
+  numbers,
+  operators,
+  setOperator,
+  updateDisplay,
+ }) => {
+    const numbersKeys = numbers.map(number => <p key={number}>{number}</p>)
+ 
+ return(
+    <div classname='keypad-container'>
+      <div className='numbers-container'>
+        {numbersKeys}
+      </div>
+    </div>
+  );
+}
 
 Keypad.propTypes ={
   callOperator: PropTypes.func.isRequired,

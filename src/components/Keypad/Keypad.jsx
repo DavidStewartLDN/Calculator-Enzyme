cdeca1e1ad43f12c2bf723b499c9d8ae86c4dda3
DavidStewartLDN <davidstewart.work@gmail.com>
2020-12-19 19:54:06 +0000

    Added Operators and test passing

--- src/components/Keypad/Keypad.jsx
+++ src/components/Keypad/Keypad.jsx
@@ -9,11 +9,16 @@ const Keypad = ({
   updateDisplay,
  }) => {
     const numbersKeys = numbers.map(number => <p key={number}>{number}</p>)
+
+    const operatorsKeys = operators.map(operator => <p key={operator}>{operator}</p>)
  
  return(
     <div classname='keypad-container'>
       <div className='numbers-container'>
         {numbersKeys}
+      </div>
+      <div className='operators-container'>
+        {operatorsKeys}
       </div>
     </div>
   );
